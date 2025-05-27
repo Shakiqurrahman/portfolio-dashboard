3979be201b98c47e6b8587c2f8285b07fde2bfd3
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-05-27 22:29:30 +0600

    url changed

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  api_url: "http://localhost:5000/api/v1",
-  // api_url: "https://portfolio-backend-topaz-eight.vercel.app/api/v1",
+  // api_url: "http://localhost:5000/api/v1",
+  api_url: "https://portfolio-backend-topaz-eight.vercel.app/api/v1",
 };
